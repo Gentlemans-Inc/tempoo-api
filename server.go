febc14ccb5c430c06dca3900f3b0e00b2b011dc8
HeavyBR <matheus.cumpian@hotmail.com>
2020-12-11 23:07:08 -0300

    Some default middlewares and grouping by api and version

--- server.go
+++ server.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
@@ -15,7 +17,16 @@ func main() {
 	//Handle Cors
 	app.Use(cors.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	//Rate limiting
+	app.Use(limiter.New())
+
+	//Handle panics
+	app.Use(recover.New())
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).SendString("Hello World")
 	})
 
