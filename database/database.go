3bed8ecb6cf31ffabd86d15a4446067fb6ad207f
HeavyBR <matheus.cumpian@hotmail.com>
2021-01-06 14:11:39 -0300

    adding sslmode to database options

--- database/database.go
+++ database/database.go
@@ -18,6 +18,7 @@ type databaseConfig struct {
 	password string
 	name     string
 	port     string
+	sslMode  string
 }
 
 var Instance *gorm.DB
@@ -28,7 +29,7 @@ func ConnectDatabase() {
 	p := dbConfig.port
 	port, err := strconv.ParseUint(p, 10, 32)
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbConfig.host, port, dbConfig.user, dbConfig.password, dbConfig.name)
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", dbConfig.host, port, dbConfig.user, dbConfig.password, dbConfig.name, dbConfig.sslMode)
 	Instance, err = gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage
@@ -59,5 +60,6 @@ func setupDatabase() *databaseConfig {
 		password: os.Getenv("DB_PASSWORD"),
 		name:     os.Getenv("DB_NAME"),
 		port:     os.Getenv("DB_PORT"),
+		sslMode:  os.Getenv("DB_SSLMODE"),
 	}
 }
